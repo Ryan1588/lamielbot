3c0f1f7eb92211311909da5e512dcc144667d140
Ryann1588 <ryaninthebox123@gmail.com>
2020-11-29 17:43:36 +0100

    Első updatae v1

---
+++ .history/bot_20201129170149.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const cfg = require('./cfg.json')
+client.on('ready' , () => 
+{
+   console.log('A bot online lett!') 
+   client.user.setActivity('Fejlesztés alatt.', {type: 'PLAYING'})
+});
+
+client.login(cfg.token)
